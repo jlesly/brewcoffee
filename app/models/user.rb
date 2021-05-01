41258d8b09a648c50167b4dd240a21b6c58c1125
jlesly <leslyj2@gmail.com>
2021-05-01 13:21:41 -0400

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :brews
+    has_many :comments
+    has_many :brew_comments, through: :brews, source: comments
+
+    validates :email, presence: true
+    validates :user_name, presence: true, uniqueness: true 
 end
