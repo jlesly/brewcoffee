93fb4c443423b185e8940e5c166124101b118251
jlesly <leslyj2@gmail.com>
2021-05-01 13:15:02 -0400

    added to brews table

--- db/migrate/20210501170558_create_brews.rb
+++ db/migrate/20210501170558_create_brews.rb
@@ -3,6 +3,11 @@ class CreateBrews < ActiveRecord::Migration[6.1]
     create_table :brews do |t|
       t.string :name
       t.string :brand
+      t.integer :grind_size
+      t.decimal :dose
+      t.decimal :extraction_volume
+      t.decimal :extraction_time
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
