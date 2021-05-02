deafc06706bcf89ed09c4bbfc50c3d2aec11ebc2
jlesly <leslyj2@gmail.com>
2021-05-02 13:13:21 -0400

    added flash message to login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
+            flash[:error] = "Login failed. Please try again."
             redirect_to '/login'
         end 
     end 
