39c5ee13199ed3adb72649098364336c56939f94
jlesly <leslyj2@gmail.com>
2021-05-02 16:14:12 -0400

    created new brew view and method in controller

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -1,2 +1,18 @@
 class BrewsController < ApplicationController
+
+    def new 
+        @brew = Brew.new
+    end 
+
+    def create
+        @brew = current_user.brews.build(brew_params)
+        if @brew.save
+            redirect_to brew_path(@brew)
+        else
+            render :new
+
+        end 
+    end
+
+    
 end
