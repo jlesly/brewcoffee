6f950505670c43df9d83e1e8f95286134db3799b
jlesly <leslyj2@gmail.com>
2021-05-07 12:51:28 -0400

    new comment form

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   
   resources :comments
   resources :brews do 
-    resources :comments, only[:new, :index]
+    resources :comments
+  end 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
