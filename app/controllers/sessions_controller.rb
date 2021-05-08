a829fe3288f8088f8cbc0dcfb40754f3534edc8c
jlesly <leslyj2@gmail.com>
2021-05-08 13:16:43 -0400

    created partial for errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,12 @@
 class SessionsController < ApplicationController
     
     def welcome
-    
+        @user = User.find_by_id(params[:id])
     end 
 
     def destroy
         session.delete :user_id
         redirect_to '/'
-    end 
-
-    def new
     end 
 
     def create
