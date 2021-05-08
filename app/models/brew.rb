a48e25ae2618c275bb7c86da2bce615136c57514
jlesly <leslyj2@gmail.com>
2021-05-08 15:42:01 -0400

    added validations for brew

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -3,4 +3,6 @@ class Brew < ApplicationRecord
     has_many :comments
     has_many :users_commented, through: :comments, source: :user
     
+    validates :name, :brand, :grind_size, :dose, :extraction_volume, :extraction_time, presence: true
+
 end
