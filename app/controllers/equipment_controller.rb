84ad7c0edacee73d4e96865fdd124ff010ff672f
jlesly <leslyj2@gmail.com>
2021-05-10 10:41:19 -0400

    created migrtion for equipment

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,2 +1,8 @@
 class EquipmentController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def index
+        @equipments = Equipment.alpha
+    end 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_171146) do
+ActiveRecord::Schema.define(version: 2021_05_10_142339) do
 
   create_table "brews", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_05_01_171146) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["brew_id"], name: "index_comments_on_brew_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "equipment", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
