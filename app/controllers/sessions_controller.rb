d8795a55ecb494d079676aed4c0f95f44321d89e
jlesly <leslyj2@gmail.com>
2021-05-12 06:05:44 -0400

    fixed sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,13 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by_id(params[:id])
-        if @user && @user.authenticate(params[:user][:password])
+    
+        if @user && @user.authenticate(password: params[:user][:password])
             session[:user_id] = @user.id 
             redirect_to user_path(@user) 
         else
             flash[:message] = "Login failed. Please try again or sign up!"
-            redirect_to '/login'
+            redirect_to login_path
         end
     end 
     
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -2,5 +2,5 @@ class Equipment < ApplicationRecord
     has_many :brews
     validates :brand_model, presence: true, uniqueness: true
 
-    scope :alpha -> {order(:brand_model)}
+    
 end
