7121af6bca287abff310fb2556ce90e60e6dacd4
jlesly <leslyj2@gmail.com>
2021-05-13 06:07:27 -0400

    fixed login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,10 @@ class SessionsController < ApplicationController
     end 
 
     def create
-        @user = User.find_by_id(params[:id])
-    
-        if @user && @user.authenticate(password: params[:user][:password])
+        @user = User.find_or_create_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id 
-            redirect_to user_path(@user) 
+            redirect_to user_path(@user)
         else
             flash[:message] = "Login failed. Please try again or sign up!"
             redirect_to login_path
